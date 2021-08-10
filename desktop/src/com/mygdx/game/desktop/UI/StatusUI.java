60c9a3867fe1451aa7f49f9685f1df21dd930d17
Genard Junsay <genard38@gmail.com>
2021-08-11 05:16:49 +0800

    2 methods done

--- desktop/src/com/mygdx/game/desktop/UI/StatusUI.java
+++ desktop/src/com/mygdx/game/desktop/UI/StatusUI.java
@@ -1,6 +1,8 @@
 package com.mygdx.game.desktop.UI;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Array;
 import com.mygdx.game.desktop.bludbourne.Utility;
 
@@ -52,6 +54,8 @@ public class StatusUI extends Window implements StatusSubject {
         Image bar = new Image(Utility.STATUSUI_TEXTUREATLAS.findRegion("Bar"));
         _mpBar = new Image(Utility.STATUS_TEXTUREATLAS.findRegion("MP_Bar"));
         Image bar2 = new Image(Utility.STATUSUI_TEXTUREATLAS.findRegion("Bar"));
+        _xpBar = new Image(Utility.STATUSUI_TEXTUREATLAS.findRegion("XP_Bar"));
+        Image bar3 = new Image(Utility.STATUSUI_TEXUREATLAS.findRegion("Bar"));
 
         _barWidth = _hpBar.getWidth();
         _barHeight = _hpBar.getHeight();
@@ -73,22 +77,233 @@ public class StatusUI extends Window implements StatusSubject {
         _inventoryButton = new ImageButton(Utility.STATUSUI_SKIN,"inventory-button");
         _inventoryButton.getImageCell().size(32,32);
 
+        _questButton = new ImageButton(Utility.STATUSUI._SKIN,"quest-button");
+        _questButton.getImageCell().size(32,32);
 
 
+        //Align images
+        _hpBar.setPosition(3,6);
+        _mpBar.setPosition(3,6);
+        _xpBar.setPosition(3,6);
+
+        //add to widget groups
+        group.addActor(bar);
+        group.addActor(_hpBar);
+        group2.addActor(bar2);
+        group2.addActor(_mpBar);
+        group3.addActor(bar3);
 
 
+        //Add to layout
+        defaults().expand().fill();
 
+        //account for the title padding
+        this.pad(this.getPadTop() + 10, 10, 10,10);
 
+        this.add();
+        this.add(_questButton).align(Align.center);
+        this.add(_inventoryButton).align(Align.right);
+        this.row();
 
+        this.add(group).size(bar.getWidth(), bar.getHeight()).padRight(10);
+        this.add(hpLabel);
+        this.add(_hpValLabel).align(Align.left);
+        this.row();
 
+        this.add(group2).size(bar2.getWidth(), bar2.getHeight()).padRight(10);
+        this.add(mpLabel);
+        this.add(_mpValLabel).align(Align.left);
+        this.row();
 
+        this.add(group3).size(bar3.getWidth(), bar3.getHeight()).padRight(10);
+        this.add(xpLabel);
+        this.add(_xpValLabel).align(Align.left).padRight(20);
+        this.row();
 
+        this.add(levelLabel).align(Align.left);
+        this.add(_levelValLabel).align(Align.left);
+        this.row();
+        this.add(goldLabel);
+        this.add(_goldValLabel).align(Align.left);
 
-
-
+        //this.debug();
+        this.pack();
 
 
     }
+
+
+    public ImageButton getInventoryButton(){ return _inventoryButton;
+    }
+
+    public ImageButton getQuestButton() {return _questButton;}
+
+    public int getLevelValue(){return _levelVal;}
+    public void setLevelValue(int levelValue){
+        this._levelVal = levelValue;
+        _levelValLabel.setText(String.valueOf(_levelVal));
+        notify(_levelVal, StatusObserver.StatusEvent.UPDATED_LEVEL);
+    }
+
+    public int getGoldValue(){return _goldVal;}
+    public void setGoldValue(int goldValue){
+        this._goldVal = goldValue;
+        _goldValLabel.setText(String.valueOf(_goldVal));
+        notify(_goldVal, StatusObserver.StatusEvent.UPDATED_GP);
+    }
+
+    public void addGoldValue(int goldValue){
+        this._goldVal += goldValue;
+        _goldValLabel.setText(String.valueOf(_goldVal));
+        notify(_goldVal, StatusObserver.StatusEvent.UPDATED_GP );
+    }
+
+    public int getXPValue(){ return _xpVal;}
+
+    public void setXPValue(int xpValue){
+        this._xpVal = xpValue;
+
+        if(_xpVal > _xpCurrentMax){
+            updateToNewLevel();
+        }
+
+        _xpValLabel.setText(String.valueOf(_xpVal));
+
+        updateBar(_xpBar, _xpVal, _xpCurrentMax);
+
+        notify(_xpVal, StatusObserver.StatusEvent.UPDATED_XP);
+    }
+
+    public void addXPValue(int xpValue){
+        this._xpVal += xpValue;
+
+        if (_xpVal > _xpCurrentMax) {
+            updateToNewLevel();
+        }
+
+        _xpValLabel.setText(String.valueOf(_xpVal));
+
+        updateBar(_xpBar, _xpVal, _xpCurrentMax );
+
+        notify(_xpVal, StatusObserver.StatusEvent.UPDATED_XP);
+    }
+
+    public void setStatusForLevel(int level){
+        for(LevelTable table: _levelTables){
+            if(Integer.parseInt(table.getlevelID()) == level ){
+                setXPValueMax(table.getXpMax());
+                setXPValue(0);
+
+                setHPValueMax(table.getHpMax());
+                setHPValue(table.getHpMax());
+
+                setMPValueMax(table.getMpMax());
+                setMPValue(table.getMpMax());
+
+                setLevelValue(Integer.parseInt(table.getLevelID()));
+                return;
+            }
+        }
+    }
+
+    public void updateToNewLevel(){
+        for (LevelTable table: _levelTables){
+            //System.out.println("XPVAL " + _xpVal + " table XPMAX " + table.getXpMax());
+            if( _xpVal > table.getXpMax()){
+                continue;
+            }else{
+                setXPValueMax(table.getXpMax());
+
+                setHPValueMax(table.getHpMax());
+                setHPValue(table.getHpMax());
+
+                setMPValueMax(table.getMpMax());
+                setMPValue(table.getMpMax());
+
+                setLevelValue(Integer.parseInt(table.getLevelID()));
+                notify(_levelVal, StatusObserver.StatusEvent.LEVELED_UP);
+                return;
+            }
+        }
+    }
+
+    public int getXPValueMax(){ return _xpCurrentMax;}
+
+    public void setXPValueMax(int maxXPValue){
+        this._xpCurrentMax = maxXPValue;
+    }
+
+    // HP
+    public int getHPValue(){return _hpVal;}
+
+    public void setHPValue(int hpValue){
+        this._hpVal = hpValue;
+        _hpValLabel.setText(String.valueOf(_hpVal));
+
+        updateBar(_hpBar, _hpVal, _hpCurrentMax);
+
+        notify(_hpVal, StatusObserver.StatusEvent.UPDATED_HP);
+    }
+
+    public void removeHPValue (int hpValue){
+        _hpVal = MathUtils.clamp(_hpVal + hpValue, 0,_hpCurrentMax );
+        _hpValLabel.setText(String.valueOf(_hpVal));
+
+        updateBar(_hpBar, _hpVal, _hpCurrentMax);
+
+        notify(_hpVal, StatusObserver.StatusEvent.UPDATED_HP);
+    }
+
+    public void addHPValue(int hpValue){
+        _hpVal = MathUtils.clamp(_hpVal + hpValue, 0, _hpCurrentMax );
+        _hpValLabel.setText(String.valueOf(_hpVal));
+
+        updatedBar(_hpBar, _hpVal, _hpCurrentMax);
+
+        notify(_hpVal, StatusObserver.StatusEvent.UPDATED_HP);
+    }
+
+    public int getHPValueMax(){return _hpCurrentMax;}
+
+    public void setHPValueMax(int maxHPValue){
+        this._hpCurrentMax = maxHPValue;
+    }
+
+   //MP
+    public int getMPValue(){return _mpVal;}
+
+
+    public void removeMPValue(int mpValue){
+        _mpVal = MathUtils.clamp(_mpVal - mpValue, 0, _mpCurrentMax );
+        _mpValLabel.setText(String.valueOf(_mpVal));
+
+        updateBar(_mpBar, _mpVal, _mpCurrentMax);
+
+        notify(_mpVal, StatusObserver.StatusEvent.UPDATED_MP);
+
+    }
+
+    public void addMPValue(int mpValue){
+        _mpVal = MathUtils.clamp(_mpVal + mpValue, 0, _mpCurrentMax);
+        _mpValLabel.setText(String.valueOf(_mpVal));
+
+
+        updateBar(_mpBar, _mpVal, _mpCurrentMax);
+
+
+        notify(_mpVal, StatusObserver.StatusEvent.UPDATED_MP);
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 
     @Override
